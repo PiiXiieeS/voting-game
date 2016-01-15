d5fd4dc25936c0d922c7000b36e36ee24bc8c6f9
CruiseDevice <achavan1211@gmail.com>
2016-01-15 21:06:09 +0530

    Completed

--- database.js
+++ database.js
@@ -1,41 +1,44 @@
-//Require the nedb module
-var Datastore = require('nedb');
+/**
+ * This file configures the data sets used in the app
+ *
+ * It uses the nedb module for storing data, and returns 
+ * an object with all the datasets. It is required in 
+ * routes.js
+ */ 
+
+
+// Require the nedb module
+var Datastore = require('nedb'),
 	fs = require('fs');
 
-//Initialize two nedb databases, Notice the autoload parameter.
-var photos = new Datastore({
-		filename:__dirname+'/data/photos',
-		autoload:true;
-	}),
-	users = new Datastore({
-		filename:__dirname+'/data/users',autoload:true
-	});
+// Initialize two nedb databases. Notice the autoload parameter.
+var photos = new Datastore({ filename: __dirname + '/data/photos', autoload: true }),
+	users = new Datastore({ filename: __dirname + '/data/users', autoload: true });
 
-//Create a unique index for the photo name and user ip
-photos.ensureIndex({fieldName:'name',unique:true});
-users.ensureIndex({fieldName:'ip',unique:true});
+// Create a "unique" index for the photo name and user ip
+photos.ensureIndex({fieldName: 'name', unique: true});
+users.ensureIndex({fieldName: 'ip', unique: true});
 
-//Load all images from the public/photos folder in the database
-var photos_on_disk = fs.readdirSync(__dirname+'/public/photos');
+// Load all images from the public/photos folder in the database
+var photos_on_disk = fs.readdirSync(__dirname + '/public/photos');
 
-
-//Insert the photos in the database, This is executed on every
-//start up of your application, but because there is a unique
-//constraint on the name field, subsequent writes will fail
-//and you will still have only one record per image:
+// Insert the photos in the database. This is executed on every 
+// start up of your application, but because there is a unique
+// constraint on the name field, subsequent writes will fail 
+// and you will still have only one record per image:
 
 photos_on_disk.forEach(function(photo){
 	photos.insert({
-		name:photo,
-		likes:0,
-		dislikes:0
-	})
+		name: photo,
+		likes: 0,
+		dislikes: 0
+	});
 });
 
-//Make the photos and users data sets available to the code
-//that users require() on this module:
+// Make the photos and users data sets available to the code
+// that uses require() on this module:
 
 module.exports = {
-	photos:photos,
-	users:users
-}
+	photos: photos,
+	users: users
+};
